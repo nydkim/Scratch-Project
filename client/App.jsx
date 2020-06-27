cabc8185fa611dbd9903d471617bff5466c8c6fd
Nak Young Kim <nydkim@gmail.com>
2020-06-27 11:00:52 -0400

    morning standup checkpoint

--- client/App.jsx
+++ client/App.jsx
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
 
-// import NavBar from './component/navigationBar/navBar.js';
+import NavBar from './component/navigationBar/navBar.js';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './component/logedIn/Login.js';
-import Canvas from './component/logedIn/canvas.jsx';
-import Home from './component/logedIn/home'
+import Canvas from './component/logedIn/canvas.js';
+import Home from './component/logedIn/home.js';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <NavBar/>
+          <NavBar />
           <Switch>
-            <Route path="/login"  component={Login}/>
-            <Route path="/canvas"  component={Canvas}/>
-            <Route path="/home"  component={Home}/>
+            <Route path="/login" component={Login} />
+            <Route path="/canvas" component={Canvas} />
+            <Route path="/home" component={Home} />
           </Switch>
         </div>
-    </Router>
-    )
+      </Router>
+    );
   }
 }
-
 
 export default App;
--- client/component/navigationBar/navBar.js
+++ client/component/navigationBar/navBar.js
@@ -1,24 +1,29 @@
-
 import React, { Component } from 'react';
-import { BrowserRouter as Router,  Switch, Route, Link} from 'react-router-dom';
-import css from '../../Style.css'
-const image = '../../image.png'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import css from '../../Style.css';
+const image = '../../image.png';
 export default class NavBar extends Component {
-  render(){
-    return(
-       <nav >
-       <img src = {image}/>
-        <ul  className= 'nav-elem'>
-          <Link to={"/Login"}><li><button className='login'>Sign in</button> </li></Link>
-          <Link to={"/canvas"}><li>Canvas</li></Link>
-          <Link to={"/"}><li>Home</li></Link>
+  render() {
+    return (
+      <nav>
+        <img src={image} />
+        <ul className="nav-elem">
+          <Link to={'/Login'}>
+            <li>
+              <button className="login">Sign in</button>{' '}
+            </li>
+          </Link>
+          <Link to={'/canvas'}>
+            <li>Canvas</li>
+          </Link>
+          <Link to={'/'}>
+            <li>Home</li>
+          </Link>
         </ul>
       </nav>
-    )
+    );
   }
 }
-
-
 
 // <div>
 //         <nav>
